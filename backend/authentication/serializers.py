d205b398b97472d9fffbdd6ca0ec30e4062b1b30
James Seelos <97702507+seelosjames@users.noreply.github.com>
2024-05-13 08:17:51 -0600

    Update serializers.py

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,10 +1,31 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
+        print("user type: ", type(user))
+        print("user: ", user)
+        print("Username: ", user.username)
 
         # Add custom claims
         token['username'] = user.username
         # ...
 
+        print("token!!!:" , token)
         return token
+    
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+    
